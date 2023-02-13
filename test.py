964a5dea4ac817f9e698ca0e74ec6a62297102c6
Raphael Nestler <raphael.nestler@renuo.ch>
2023-02-13 13:49:59 +0100

    Add two more cases

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+from typing import Union
+
 this_is_fine = {
     "bar": True,
     "value_0": 1.0,
@@ -8,3 +11,13 @@ this_is_not = {
     "bar": True,
     **{f"value_{n}": float(n) for n in range(2)},
 }
+
+this_is_ok_again: dict[str, Union[bool, float]] = {
+    "bar": True,
+    **{f"value_{n}": float(n) for n in range(2)},
+}
+
+but_this_not_ok_again: OrderedDict[str, Union[bool, float]] = OrderedDict({
+    "bar": True,
+    **{f"value_{n}": float(n) for n in range(2)},
+})
